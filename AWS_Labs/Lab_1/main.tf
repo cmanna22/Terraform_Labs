bab62537b790f59d574ede105c77233482b5eabb
Chauncey <chaunceycrowder9@gmail.com>
2026-06-17 00:31:17 -0400

    added git ignore

---
+++ AWS_Labs/Lab_1/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "sample" {
+    ami = "ami-0c94855ba95c71c99"
+    instance_type = "t2.micro"
+
+
+    tags = {
+        name = "sample-instance"
+    } 
+}
